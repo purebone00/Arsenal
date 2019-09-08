c149840043d639d48a187e753a2dd73b8c1dfd5c
Albert <purebone00@gmail.com>
2019-09-07 23:18:47 -0700

    Created Initial Transaction Models, and Created Transaction Spreadsheet

---
+++ Arsenal/Models/ArsenalDataModels/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arsenal.Models.ArsenalDataModels
+{
+    public class Transaction
+    {
+        [Key] 
+        public int TransactionID { get; set; }
+
+        public DateTime TransactionDate { get; set; }
+        public string Description { get; set; }
+
+        public int TotalAmount { get; set; }
+
+        public int GstAmount { get; set; }
+
+        public int PstAmount { get; set; }
+
+        public string PaymentType { get; set; }
+
+    }
+}
