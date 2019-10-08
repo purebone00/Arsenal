462eef625eee3c16321e7a41818db9ec8567d43e
Albert Chen <purebone00@gmail.com>
2019-10-07 19:32:24 -0700

    Discovered Bug when saving after editing a listing, causing it to null out the creditcardlastfourdigits property in the database. Should be fixed

--- Arsenal/wwwroot/Controllers/TransactionsController.cs
+++ Arsenal/wwwroot/Controllers/TransactionsController.cs
@@ -98,7 +98,7 @@ namespace Arsenal.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("TransactionID,TransactionDate,Description,TotalAmount,GstAmount,PstAmount,PaymentType")] Transaction transaction)
+        public async Task<IActionResult> Edit(int id, [Bind("TransactionID,TransactionDate,Description,TotalAmount,GstAmount,PstAmount,PaymentType,CreditCardLastFourDigits")] Transaction transaction)
         {
             if (id != transaction.TransactionID)
             {
